4790096c33f65a4802ebf11fe31dcf295f3847a0
Owen <owenb2528@gmail.com>
2025-02-19 09:08:32 -0500

    fixed camera switching

--- Assets/Scripts/Camera controller.cs
+++ Assets/Scripts/Camera controller.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class Cameracontroller : MonoBehaviour
 {
     public Camera MenuCamera;
-    public Camera Player;
+    public Camera PlayerCamera;
+    public Difficulty Difficulty;
     // Start is called before the first frame update
     void Start()
     {
         MenuCamera.enabled = true;
-        Player.enabled = false;
+        PlayerCamera.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Difficulty.easy == true)
+        {
+            MenuCamera.enabled = false;
+            PlayerCamera.enabled = true;
+        }
+
+        if (Difficulty.medium == true)
+        {
+            MenuCamera.enabled = false;
+            PlayerCamera.enabled = true;
+        }
+
+        if (Difficulty.hard == true)
+        {
+            MenuCamera.enabled = false;
+            PlayerCamera.enabled = true;
+        }
+
+        if (Difficulty.insane == true)
+        {
+            MenuCamera.enabled = false;
+            PlayerCamera.enabled = true;
+        }
     }
 }
