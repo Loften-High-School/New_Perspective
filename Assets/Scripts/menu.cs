6bc726a3d094c1eeab508325eb9751ec8505b963
Owen <owenb2528@gmail.com>
2025-02-20 09:50:13 -0500

    hue hue hue hueeee

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class menu : MonoBehaviour
 {
-    private Rigidbody2D rb;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Debug.Log("Play Button was clicked");
-            }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log ("Play button clicked");
+        }
     }
 }
