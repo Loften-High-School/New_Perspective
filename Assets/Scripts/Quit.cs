a9d35ee68fbeb81607dcfba3674cfbee89c0dc50
Owen <owenb2528@gmail.com>
2025-04-18 09:14:05 -0400

    Made and finished a quit button

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+[SerializeField] public GameObject button;
+    private Rigidbody2D rb;
+    public bool Clicked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMouseDown()
+    {
+        Clicked = true;
+    }
+
+    void Update()
+    {
+        
+    }
+}
